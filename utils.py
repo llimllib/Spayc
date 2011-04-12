9f148a2bf32b95e8465834b3f6938de317a98aa6
bill <bill.mill@gmail.com>
2011-04-11 22:45:23 -0400

    add param handling to send

--- utils.py
+++ utils.py
@@ -21,10 +21,14 @@ def query(sql, *params):
 def send(topic_id, message, **params):
     """Send a message in a thread-safe manner"""
     p("trying to send: %s to %s" % (message, topic_id))
-    #does this make a request?
+
     conv_auth = requests.AuthObject(config.username, config.password)
+
+    data = {"message": message.encode('utf-8')}
+    data.update(params)
+
     r = requests.post("https://convore.com/api/topics/%s/messages/create.json" % topic_id,
-                      data={"message": message.encode('utf-8')}.update(params), auth=conv_auth)
+                      data=data, auth=conv_auth)
 
     assert r.status_code == 200
     p("successful send")
